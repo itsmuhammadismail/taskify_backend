a190ef4c48098a7d4af86e2402ed4fdb072b506e
Muhammad Ismail <itsmuhammadismail@gmail.com>
2023-11-19 12:49:28 +0500

    new changes

--- routers/tasks_history.py
+++ routers/tasks_history.py
@@ -4,7 +4,7 @@ from schemas.task_history import task_history_entity, tasks_history_entity
 from fastapi.responses import JSONResponse
 from bson.objectid import ObjectId
 from models.task_history import TaskHistory
-from datetime import datetime
+from datetime import datetime, timedelta
 
 router = APIRouter(
     prefix="/tasks_history",
@@ -109,3 +109,48 @@ async def delete_task_history(id: str):
     db.task_history.find_one_and_delete({"_id": ObjectId(id)})
 
     return {"message": "task deleted successfully"}
+
+
+@router.get("/graph/{id}")
+async def get_graph_data(id: str):
+    dateTimeList = list(map(lambda n: datetime.now() -
+                        timedelta(minutes=n), [0, 15, 30, 45, 60, 75]))
+    counts = [0 for date in dateTimeList]
+
+    lookup_stage = {
+        "$lookup": {
+            "from": "tasks",  # the collection to join
+            "localField": "task",  # the field from the input documents
+            "foreignField": "_id",  # the field from the joined documents
+            "as": "task_details"  # the output array field
+        }
+    }
+
+    match_stage = {
+        "$match": {
+            "user": ObjectId(id)
+        }
+    }
+
+    tasks_data = db.task_history.aggregate([match_stage, lookup_stage])
+
+    tasks = tasks_history_entity(tasks_data)
+
+    for index, time in enumerate(dateTimeList):
+        for task in tasks:
+            start_time = datetime.strptime(
+                task["start_time"], "%Y-%m-%d %H:%M:%S.%f")
+            if start_time <= time:
+                if task["end_time"] == None:
+                    counts[index] += 1
+                else:
+                    end_time = datetime.strptime(
+                        task["end_time"], "%Y-%m-%d %H:%M:%S.%f")
+
+                    if end_time <= time and end_time >= (time - timedelta(minutes=15)):
+                        counts[index] += 1
+
+    return {
+        "dateTimeList": dateTimeList,
+        "counts": counts
+    }

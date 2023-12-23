a6300d7623b7cbe6a0ac6faa05e10250c02be848
Muhammad Ismail <itsmuhammadismail@gmail.com>
2023-12-23 16:30:55 +0500

    ai modal update

--- ai_model/index.py
+++ ai_model/index.py
@@ -6,28 +6,28 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 
 def recommender(running_tasks, todo_tasks):
-    todo_tasks.insert(0, running_tasks[0])
-    running_tasks_df = pd.DataFrame(running_tasks)
-    todo_tasks_df = pd.DataFrame(todo_tasks)
-
-    # running_tasks_df = running_tasks_df[['id', 'desc']]
-    # todo_tasks_df = todo_tasks_df[['id', 'desc']]
-
-    cv = CountVectorizer(max_features=2500, stop_words='english')
-    vector = cv.fit_transform(todo_tasks_df['desc']).toarray()
-
-    similarity = cosine_similarity(vector)
-
-    # print(similarity)
-    # print(todo_tasks_df['desc']
-    #       == running_tasks[0]['desc'])
-
-    index = todo_tasks_df[todo_tasks_df['desc']
-                          == running_tasks[0]['desc']].index[0]
-    distances = sorted(
-        list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
-
     try:
+        todo_tasks.insert(0, running_tasks[0])
+        running_tasks_df = pd.DataFrame(running_tasks)
+        todo_tasks_df = pd.DataFrame(todo_tasks)
+
+        # running_tasks_df = running_tasks_df[['id', 'desc']]
+        # todo_tasks_df = todo_tasks_df[['id', 'desc']]
+
+        cv = CountVectorizer(max_features=2500, stop_words='english')
+        vector = cv.fit_transform(todo_tasks_df['desc']).toarray()
+
+        similarity = cosine_similarity(vector)
+
+        # print(similarity)
+        # print(todo_tasks_df['desc']
+        #       == running_tasks[0]['desc'])
+
+        index = todo_tasks_df[todo_tasks_df['desc']
+                              == running_tasks[0]['desc']].index[0]
+        distances = sorted(
+            list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+
         recommended_task_df = todo_tasks_df.iloc[distances[1][0]]
         recommended_tasks = recommended_task_df.to_dict()
         return recommended_tasks
